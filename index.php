1dfd672bc59cf4b1d718df5369221036929f9d10
JF-BimCopinski <jfbcdev@gmail.com>
2026-07-29 22:18:41 -0300

    Add basic HTML structure in index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'inc/header.inc.php'; ?>
+
+    <h1> Aqui será o corpo do site! </h1>
+    <h1> Como criar uma estrutura básica de uma página HTML? </h1>
+    <h1> Resposta: digitar "!" e escolher a primeira opção, automaticamente será gerada o DOCTYPE para você </h1>
+
+<?php include 'inc/footer.inc.php'; ?>
